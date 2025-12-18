eaf7ed4a774b61a6b1e8d74342644606a9023c43
Hritik1503 <1503hritik.s@gmail.com>
2025-12-18 23:06:21 +0530

    getprisma

--- lib/server/user.ts
+++ lib/server/user.ts
@@ -3,7 +3,7 @@ import { getPrisma } from "@/lib/prisma";
 
 export async function findUserByEmail(email: string) {
   const prisma = getPrisma();
-
+console.log(prisma);
   return prisma.user.findUnique({
     where: { email },
   });
