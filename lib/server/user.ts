e282bed1e62e96de7ee7bd3796fc489bcc759dea
Hritik1503 <1503hritik.s@gmail.com>
2025-12-18 21:20:59 +0530

    push

---
+++ lib/server/user.ts
@@ -0,0 +1,8 @@
+import "server-only";
+import { prisma } from "@/lib/prisma";
+
+export async function findUserByEmail(email: string) {
+  return prisma.user.findUnique({
+    where: { email },
+  });
+}
